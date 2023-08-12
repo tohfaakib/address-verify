1eb4fa70ef7024af52bbdb30deb77eb63eb7e704
Tohfa Akib <tnishan7@gmail.com>
2023-08-12 23:58:48 +0600

    added property api

--- app.js
+++ app.js
@@ -27,8 +27,42 @@ app.post('/get_data', async (req, res) => {
     const response = await axios.get(url);
     // const response = await axios.get(url, { params: querystring });
 
-    console.log(response.data);
-    res.json(response.data);
+    // console.log(response.data);
+
+    console.log("++++++++++++++++++++++++++++++++++++++++++");
+
+    // let property_res = ""
+    // let config = {
+    //   method: 'get',
+    //   maxBodyLength: Infinity,
+    //   url: 'https://property.melissadata.net/v4/WEB/LookupProperty/?id=biSxhdpkI8-4KVqfEHnJ_H**nSAcwXpxhQ0PC2lXxuDAZ-**&ff=3208 Berkshire Way, Sacramento, CA 95864&format=json',
+    //   headers: { }
+    // };
+    
+    // axios.request(config)
+    // .then((pro_response) => {
+    //   console.log(JSON.stringify(pro_response.data));
+    //   property_res = JSON.stringify(pro_response.data);
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // });
+
+    const pro_url = 'https://property.melissadata.net/v4/WEB/LookupProperty/?id=biSxhdpkI8-4KVqfEHnJ_H**nSAcwXpxhQ0PC2lXxuDAZ-**&ff=3208 Berkshire Way, Sacramento, CA 95864&format=json'
+
+    const property_res = await axios.get(pro_url);
+
+    console.log("property:", property_res.data);
+
+
+    let all_data = {
+      "usgeocoder": response.data,
+      "melissa": property_res.data
+    }
+
+    console.log("all:", all_data);
+
+    res.json(all_data);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Something went wrong" });
