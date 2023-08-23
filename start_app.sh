30bb36f68f449ba12e13bbcc6553022364ae5522
Tohfa Akib <tnishan7@gmail.com>
2023-08-23 20:05:46 +0600

    start app script added

---
+++ start_app.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Get the current directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Update system packages
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker.io
+
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Set up UFW (Uncomplicated Firewall)
+sudo apt-get install -y ufw
+#sudo ufw allow 22/tcp     # SSH
+sudo ufw allow 3000/tcp   # Your app's port
+sudo ufw enable
+
+# Build Docker image
+docker build -t address_verify "$SCRIPT_DIR"
+
+# Install Docker Compose
+sudo apt-get install -y docker-compose
+
+# Start the app using Docker Compose
+docker-compose up -d
+
+# Add a crontab entry to start the app on reboot
+(crontab -l ; echo "@reboot cd \"$SCRIPT_DIR\" && docker-compose up -d") | crontab -
+
+echo "Setup completed! Your app should be up and running after reboot."
