61c216835e4f3cafcaea10ba6b307c2f7a4158e2
Tohfa Akib <tnishan7@gmail.com>
2023-08-25 16:48:08 +0600

    reverse proxy is added to script

--- start_app.sh
+++ start_app.sh
@@ -16,6 +16,7 @@ sudo systemctl enable docker
 # Set up UFW (Uncomplicated Firewall)
 sudo apt-get install -y ufw
 #sudo ufw allow 22/tcp     # SSH
+sudo ufw allow 80/tcp     # HTTP for Nginx
 sudo ufw allow 3000/tcp   # Your app's port
 sudo ufw enable
 
@@ -28,7 +29,34 @@ sudo apt-get install -y docker-compose
 # Start the app using Docker Compose
 sudo docker-compose up -d
 
-# Add a crontab entry to start the app on reboot
-(crontab -l ; echo "@reboot cd \"$SCRIPT_DIR\" && sudo docker-compose up -d") | crontab -
+# Install and configure Nginx
+sudo apt-get install -y nginx
 
-echo "Setup completed! Your app should be up and running after reboot."
+# Ask user for server IP address
+read -p "Enter your server's public IP address: " SERVER_IP
+
+# Configure Nginx as a reverse proxy
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOT
+server {
+    listen 80;
+    server_name $SERVER_IP;
+
+    location / {
+        proxy_pass http://127.0.0.1:3000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    }
+}
+EOT
+
+# Test Nginx configuration
+sudo nginx -t
+
+# Reload Nginx
+sudo systemctl reload nginx
+
+# Add a crontab entry to start the app and Nginx on reboot
+(crontab -l ; echo "@reboot cd \"$SCRIPT_DIR\" && sudo docker-compose up -d && sudo systemctl start nginx") | crontab -
+
+echo "Setup completed! Your app and Nginx reverse proxy should be up and running after reboot."
